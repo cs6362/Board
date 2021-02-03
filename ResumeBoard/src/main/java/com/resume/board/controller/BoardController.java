2b25fe8a2a13bb51835fc086975c45f71025565c
cs_63 <cs_63@192.168.219.104>
2021-02-03 22:11:48 +0900

    [2] Controller write

---
+++ ResumeBoard/src/main/java/com/resume/board/controller/BoardController.java
@@ -0,0 +1,24 @@
+package com.resume.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+
+	
+
+	@RequestMapping("/")
+	public String index() {
+		return "home";
+	}
+	
+	
+}
+
+
+
+
+
+
+
